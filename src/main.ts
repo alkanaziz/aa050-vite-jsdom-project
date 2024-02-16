e11c657df408fc7c16b2bd8d742f14f1a091c31a
alkanaziz <alkanaziz@gmail.com>
2024-02-16 10:56:24 +0100

    sass installed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import './style.css'
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
 
     <h1>Vite + TypeScript </h1>
     <p>Welcome to your Vite + TypeScript app</p>
-
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+    </ul>
 `;
