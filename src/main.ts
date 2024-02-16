72474a616d68ccc749069e68781d47cb61f5d54c
alkanaziz <alkanaziz@gmail.com>
2024-02-16 11:13:04 +0100

    tailwind installed

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import "./style.scss";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
 
-    <h1>Vite + TypeScript </h1>
+    <h1 class="text-2xl mb-3">Vite + TypeScript </h1>
     <p>Welcome to your Vite + TypeScript app</p>
-    <ul>
+    <ul class="list-disc ml-6">
       <li>One</li>
       <li>Two</li>
       <li>Three</li>
