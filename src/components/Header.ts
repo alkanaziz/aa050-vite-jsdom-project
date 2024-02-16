f56c54fa11695b21b75e34cce2e62306102c2ca9
alkanaziz <alkanaziz@gmail.com>
2024-02-16 12:11:06 +0100

    Header as component created

---
+++ src/components/Header.ts
@@ -0,0 +1,6 @@
+export const Header = () => {
+  return /*html*/`
+    <h1 class="text-2xl mb-3">Vite + TypeScript </h1>
+    <p>Welcome to your Vite + TypeScript app</p>
+  `;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import "./style.scss";
 import skills from "./data/skills.json";
+import { Header } from "./components/Header";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
-
-    <h1 class="text-2xl mb-3">Vite + TypeScript </h1>
-    <p>Welcome to your Vite + TypeScript app</p>
-
+    ${Header()}
     <h2 class="text-xl mt-4 text-yellow-300">${skills.length} Web Developer Skills</h2>
     <ul class="list-disc ml-6">
       ${skills.map((skill) => {
           return /*html*/`<li><span class="text-orange-400 font-semibold">${skill.name}</span> - ${skill.description}</li>`;
       }).join("")}
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
     </ul>
 `;
